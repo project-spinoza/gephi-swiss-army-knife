a23ac42d187267217142a32efee8d3d90c58a2e8
bit-whacker <m.sadiq.khan92@gmail.com>
2016-01-04 15:34:14 -0500

    added src and pom.xml

---
+++ src/main/java/org/projectspinoza/gephiswissarmyknife/Main.java
@@ -0,0 +1,21 @@
+package org.projectspinoza.gephiswissarmyknife;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Main
+ * 
+ * @author org.projectspinoza
+ * @version 1.0.0
+ *
+ */
+public class Main 
+{
+	private static Logger log = LogManager.getLogger(Main.class);
+	
+    public static void main( String[] args )
+    {
+        log.info( "Hello World!" );
+    }
+}
