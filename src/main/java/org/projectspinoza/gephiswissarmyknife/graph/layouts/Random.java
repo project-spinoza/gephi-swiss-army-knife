b3613d24b03cd3f9be30fd318f99444e69d6b17a
kowalski100 <ijaz.a100@yahoo.com>
2016-02-05 17:15:39 -0800

    Random Layout ingegrated

---
+++ src/main/java/org/projectspinoza/gephiswissarmyknife/graph/layouts/Random.java
@@ -0,0 +1,48 @@
+package org.projectspinoza.gephiswissarmyknife.graph.layouts;
+
+import java.util.Map;
+
+import org.gephi.graph.api.GraphModel;
+import org.gephi.layout.plugin.random.RandomLayout;
+
+public class Random {
+
+  private RandomLayout randomLayout;
+  private GraphModel graphModel;
+  
+  public Random() {
+  }
+  
+  public void applyLayout (Map <String, String> layoutParams) {
+    this.randomLayout = (this.randomLayout == null) ? new RandomLayout(null, 0.0) : this.randomLayout;
+    this.randomLayout.setGraphModel(this.graphModel);
+    this.randomLayout.resetPropertiesValues();
+    randomLayout(layoutParams);
+  }
+  
+  private void randomLayout(Map <String, String> layoutParams) {
+    this.randomLayout.setConverged(true);
+    this.randomLayout.setSize(Double.parseDouble(layoutParams.get("space_size")));
+    this.randomLayout.initAlgo();
+    for (int i = 0; i < 100 & this.randomLayout.canAlgo() ; i++ ) {
+      this.randomLayout.goAlgo();
+    }
+    this.randomLayout.endAlgo();
+  }
+
+  public RandomLayout getRandomLayout() {
+    return randomLayout;
+  }
+
+  public void setRandomLayout(RandomLayout randomLayout) {
+    this.randomLayout = randomLayout;
+  }
+
+  public GraphModel getGraphModel() {
+    return graphModel;
+  }
+
+  public void setGraphModel(GraphModel graphModel) {
+    this.graphModel = graphModel;
+  }
+}
